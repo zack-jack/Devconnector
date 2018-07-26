485d714ba5248ced2e62da2ac2600c0f6c2bef9a
Zack Jackson <z.jackson58@gmail.com>
2018-07-26 14:48:25 -0400

    added initial route files

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+
 const app = express();
 
 // MongoDB Config
@@ -17,6 +21,11 @@ mongoose
 
 // Main route
 app.get("/", (req, res) => res.send("Hello"));
+
+// Use routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 // Sets up port for heroku later || 5000 for local dev now
 const port = process.env.PORT || 5000;
