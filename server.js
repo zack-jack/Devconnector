99c114f8c5b08a174fa41f80667dc0fed477246f
Zack Jackson <z.jackson58@gmail.com>
2018-07-26 12:37:07 -0400

    node.js server and dependencies setup

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Hello"));
 
+//sets up port for heroku later || 5000 for local dev now
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
