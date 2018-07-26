9bef9bec28759f7029068df5de674b5129d5764b
Zack Jackson <z.jackson58@gmail.com>
2018-07-26 14:15:41 -0400

    added mongoDB connection via mongoose

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://admin:devadmin1@ds253821.mlab.com:53821/dev_connector_zj"
+};
--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
+// MongoDB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB with mongoose
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+// Main route
 app.get("/", (req, res) => res.send("Hello"));
 
-//sets up port for heroku later || 5000 for local dev now
+// Sets up port for heroku later || 5000 for local dev now
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
