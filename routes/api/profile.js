3e904146e2f188cda36de874c8fe14b07cb4a082
Zack Jackson <z.jackson58@gmail.com>
2018-07-27 21:14:37 -0400

    Delete user and profile capability added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -288,4 +288,19 @@ router.delete(
   }
 );
 
+// @route   DELETE api/profile
+// @desc    Delete User and Profile
+// @access  Private
+router.delete(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOneAndRemove({ user: req.user.id }).then(() => {
+      User.findOneAndRemove({ _id: req.user.id }).then(() =>
+        res.json({ success: true })
+      );
+    });
+  }
+);
+
 module.exports = router;
