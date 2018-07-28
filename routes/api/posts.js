2e75551b279b49500d24ae6a9e8a5d0a6bdb39be
Zack Jackson <z.jackson58@gmail.com>
2018-07-27 22:47:04 -0400

    Get routes for all posts or post by ID.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -14,6 +14,27 @@ const validatePostInput = require("../../validation/post");
 // @access  Public
 router.get("/test", (req, res) => res.json({ msg: "Posts route works" }));
 
+// @route   GET api/posts
+// @desc    Get posts
+// @access  Public
+router.get("/", (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json({ nopostsfound: "No posts found." }));
+});
+
+// @route   GET api/posts/:id
+// @desc    Get posts by id
+// @access  Public
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err =>
+      res.status(404).json({ nopostfound: "No post found with that ID." })
+    );
+});
+
 // @route   POST api/posts
 // @desc    Create post
 // @access  Private
