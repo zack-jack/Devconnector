7dac5de1c2ea274298ed2ef3ec82b2519fbb6247
Zack Jackson <z.jackson58@gmail.com>
2018-08-02 17:12:39 -0400

    keys

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,5 @@
-module.exports = {
-  // MongoDB mLab url
-  mongoURI:
-    "mongodb://admin:devadmin1@ds253821.mlab.com:53821/dev_connector_zj",
-  // secret key for jwt
-  // see Sign Token in users.js
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
